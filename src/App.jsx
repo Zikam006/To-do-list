c8f759b255578f4304b8107ce0d0d20be5994af8
lilif <zikeanyibe@gmail.com>
2024-03-19 11:30:31 +0100

    my learnable react t-do list

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from "./components/nav"
+
+function App () {
+  return (
+    <>
+    <Nav/>
+    <div>My name is zita</div>
+    <div></div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/nav.jsx
@@ -0,0 +1,20 @@
+import "./nav.css"
+
+function Nav () {
+    return(
+        <>
+        <header>
+            <nav>
+                <h3>Logo</h3>
+                <ul className="Daniel">
+                  <li>Home</li>
+                  <li>About me</li>
+                  <li>Feedback</li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default Nav
